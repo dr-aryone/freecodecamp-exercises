4bc5198585de874d44880d1081b4aa7e0777b341
Phillip Shim <shimphillip@gmail.com>
2018-07-16 21:09:45 -0500

    gettin there

---
+++ Javascript Algorithms And Data Structures Certification/Functional Programming/Refactor Global Variables Out of Functions.js
@@ -0,0 +1,40 @@
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function add (arr, bookName) {
+  
+  let copyBookList = [...arr];
+
+  copyBookList.push(bookName);
+
+  return copyBookList;
+
+  // Add your code above this line
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function remove (arr, bookName) {
+
+  let copyBookList = [...arr];
+
+  if (copyBookList.indexOf(bookName) >= 0) {
+    copyBookList.splice(copyBookList.indexOf(bookName), 1);
+    
+    return copyBookList
+    
+    // Add your code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(newestBookList);
