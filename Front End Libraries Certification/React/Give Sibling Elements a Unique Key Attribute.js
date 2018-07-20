e149741147c52ec3d25db27e25530e3dccf03dea
Phillip Shim <shimphillip@gmail.com>
2018-07-19 19:31:27 -0500

    React

---
+++ Front End Libraries Certification/React/Give Sibling Elements a Unique Key Attribute.js
@@ -0,0 +1,23 @@
+
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((val, index) => {
+    return <li key={index}>{val}</li>
+  }); // change code here
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
