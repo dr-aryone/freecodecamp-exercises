f2bacf91fd1e2aec1cedf3583ffe7c1d114bacc6
Phillip Shim <shimphillip@gmail.com>
2018-07-19 22:34:16 -0500

    Redux

---
+++ Front End Libraries Certification/Redux/Never Mutate State.js
@@ -0,0 +1,32 @@
+const ADD_TO_DO = 'ADD_TO_DO';
+
+// A list of strings representing tasks to do:
+const todos = [
+  'Go to the store',
+  'Clean the house',
+  'Cook dinner',
+  'Learn to code',
+];
+
+const immutableReducer = (state = todos, action) => {
+  switch(action.type) {
+    case ADD_TO_DO:
+      let arr = state.map(x => x);
+      arr.push(action.todo);
+      return arr;
+    break;
+      // don't mutate state here or the tests will fail
+    default:
+      return state;
+  }
+};
+
+// an example todo argument would be 'Learn React',
+const addToDo = (todo) => {
+  return {
+    type: ADD_TO_DO,
+    todo: todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
