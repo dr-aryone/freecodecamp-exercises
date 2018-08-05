2e4d2530cd63cb6835f86d455fbcb99d07281cb4
shimphillip <shimphillip@gmail.com>
2018-08-05 11:42:18 -0500

    Adding footer

--- Front End Libraries Certification/Front End Libraries Projects/buildarandomquotemachine/src/App.js
+++ Front End Libraries Certification/Front End Libraries Projects/buildarandomquotemachine/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import QuoteBox from './components/quote-box.js'
+import Footer from './components/footer.js'
 
 class App extends Component {
   render() {
     return (
       <div id="wrapper">
         <QuoteBox />
+        <Footer />
       </div>
     );
   }
