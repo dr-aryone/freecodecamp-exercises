9e16061218d33260e1dcf408de8f35c7769c6807
shimphillip <shimphillip@gmail.com>
2018-08-05 10:46:24 -0500

    Final Version

--- Front End Libraries Certification/Front End Libraries Projects/buildarandomquotemachine/src/components/quote-box.js
+++ Front End Libraries Certification/Front End Libraries Projects/buildarandomquotemachine/src/components/quote-box.js
@@ -28,23 +28,19 @@ class QuoteBox extends Component {
 
   render() {
     return (
-      <div className='container' id="quote-box">
-        <div className='row'>
-          <div className = "col-2">
-          </div>
-          <div className = "col-8">
+      <div className='container-fluid' id="quote-box">
             <blockquote className="blockquote">
-              <p className="mb-0" id="text">
-                {this.state.quote}
+              <p className= "lead text-right" id="text">
+              <i className="fas fa-quote-left"></i> {this.state.quote} <i className="fas fa-quote-right"></i>
               </p>
-              <footer className="blockquote-footer" id="author">
+              <footer className="blockquote-footer text-right" id="author">
                 <cite title="Source Title">{this.state.author}</cite>
               </footer> 
             </blockquote>
-          </div>
-        </div>
-          <a className ="btn btn-outline-info btn-lg btn-block" href={"https://twitter.com/intent/tweet?related=freecodecamp&text=" + encodeURIComponent('"' + this.state.quote + '" - ' + this.state.author)} id="tweet-quote" target="_blank"><i className="fab fa-twitter"></i> THIS</a>
-          <button type="button" className="btn btn-outline-success btn-lg btn-block" id="new-quote" onClick={this.getQuote}>ANOTHER <i className="fas fa-quote-left"></i></button>
+            <div id = "links">
+          <a className ="btn btn-outline-info" href={"https://twitter.com/intent/tweet?related=freecodecamp&text=" + encodeURIComponent('"' + this.state.quote + '" - ' + this.state.author)} id="tweet-quote" target="_blank"><i className="fab fa-twitter"></i></a>&nbsp;
+          <button type="button" className="btn btn-outline-success" id="new-quote" onClick={this.getQuote}>New <i className="fas fa-quote-left"></i></button>
+            </div>
       </div>
     )
   }
