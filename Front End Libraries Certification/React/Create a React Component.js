4ce2668dd115631ffd9be53136d5ffc97d998095
Phillip Shim <shimphillip@gmail.com>
2019-02-20 15:13:42 -0600

    Update Create a React Component.js

--- Front End Libraries Certification/React/Create a React Component.js
+++ Front End Libraries Certification/React/Create a React Component.js
@@ -6,12 +6,8 @@ class MyComponent extends React.Component {
   render() {
     // change code below this line
     return (
-      <div>
-        <h1>Hello React!</h1>
-      </div>
+      <div><h1>Hello React!</h1></div>
     );
-
-
     // change code above this line
   }
 };
