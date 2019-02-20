ef81eda812b7928d4c3cca45bf08c89419291b54
Phillip Shim <shimphillip@gmail.com>
2019-02-20 15:04:20 -0600

    Update Create a Complex JSX Element.js

--- Front End Libraries Certification/React/Create a Complex JSX Element.js
+++ Front End Libraries Certification/React/Create a Complex JSX Element.js
@@ -1,11 +1,12 @@
 // write your code here
-const JSX = <div>
-    <h1></h1>
-    <p></p>
-    <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        
-    </ul>
-</div>
+const JSX=(
+    <div>
+        <h1>What is React?</h1>
+        <p>JavaScript framework by Facebook</p>
+        <ul>
+            <li>Use of Virtual DOM</li>
+            <li>Use of View of MVC</li>
+            <li>Very performant</li>
+        </ul>
+    </div>
+);
