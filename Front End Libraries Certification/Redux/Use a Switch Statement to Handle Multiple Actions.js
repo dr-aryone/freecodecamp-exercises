bec0c3340110790d7b7774fa01831934282e6ca7
Phillip Shim <shimphillip@gmail.com>
2019-02-21 09:39:43 -0600

    Update Use a Switch Statement to Handle Multiple Actions.js

--- Front End Libraries Certification/Redux/Use a Switch Statement to Handle Multiple Actions.js
+++ Front End Libraries Certification/Redux/Use a Switch Statement to Handle Multiple Actions.js
@@ -4,19 +4,16 @@ const defaultState = {
 
 const authReducer = (state = defaultState, action) => {
   // change code below this line
-  switch(action.type){
-    case "LOGIN" :
+  switch(action.type) {
+    case "LOGIN":
       return {
         authenticated: true
       }
-    break;
-    case "LOGOUT" :
+    case "LOGOUT":
       return {
         authenticated: false
       }
-      break;
-    default:
-      return state;
+    default: return state;
   }
   // change code above this line
 };
