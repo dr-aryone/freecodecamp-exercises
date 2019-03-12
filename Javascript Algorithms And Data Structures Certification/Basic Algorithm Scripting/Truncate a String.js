7204635f225d3775e010261894ec556119002514
Phillip Shim <shimphillip@gmail.com>
2019-03-12 12:11:35 -0500

    Update Truncate a String.js

--- Javascript Algorithms And Data Structures Certification/Basic Algorithm Scripting/Truncate a String.js
+++ Javascript Algorithms And Data Structures Certification/Basic Algorithm Scripting/Truncate a String.js
@@ -1,9 +1,7 @@
 function truncateString(str, num) {
-  // Clear out that junk in your trunk
-  if(num >= str.length) 
-    return str;
-    
-  return str.slice(0, num) + "...";
+  return str.length <= num ? str : str.slice(0, num) + '...';
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+// truncateString("Peter Piper picked a peck of pickled peppers", 11)
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length)
