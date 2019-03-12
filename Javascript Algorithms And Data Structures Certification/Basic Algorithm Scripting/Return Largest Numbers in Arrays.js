35321c27079310d8bae468c8ad8f961fa4cbd5f4
Phillip Shim <shimphillip@gmail.com>
2019-03-12 11:15:59 -0500

    Update Return Largest Numbers in Arrays.js

--- Javascript Algorithms And Data Structures Certification/Basic Algorithm Scripting/Return Largest Numbers in Arrays.js
+++ Javascript Algorithms And Data Structures Certification/Basic Algorithm Scripting/Return Largest Numbers in Arrays.js
@@ -1,12 +1,5 @@
 function largestOfFour(arr) {
-  // You can do this!
-  let newArr = [];
-
-  for(let i =0;i<arr.length; i++){
-    newArr.push(Math.max(...arr[i]));
-  }
-
-  return newArr;
+  return arr.map(subArr => Math.max(...subArr));
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
