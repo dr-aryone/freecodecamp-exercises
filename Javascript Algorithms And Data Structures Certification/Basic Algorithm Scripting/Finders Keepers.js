f422d7ffaea6783fe4fed9869524a7c4be76009c
Phillip Shim <shimphillip@gmail.com>
2019-03-12 12:15:58 -0500

    Update Finders Keepers.js

--- Javascript Algorithms And Data Structures Certification/Basic Algorithm Scripting/Finders Keepers.js
+++ Javascript Algorithms And Data Structures Certification/Basic Algorithm Scripting/Finders Keepers.js
@@ -1,9 +1,5 @@
 function findElement(arr, func) {
-  for(let i=0; i<arr.length; i++){
-    if(func(arr[i])) return arr[i];
-  }
-
-  return undefined;
+  return arr.filter(func)[0];
 }
 
 findElement([1, 2, 3, 4], num => num % 2 === 0);
