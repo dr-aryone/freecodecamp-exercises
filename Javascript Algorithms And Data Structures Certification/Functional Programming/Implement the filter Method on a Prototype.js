c4788609fcdc8b71939bc4cffae54f8eaf2bb1a3
Phillip Shim <shimphillip@gmail.com>
2019-03-13 16:14:03 -0500

    Update Implement the filter Method on a Prototype.js

--- Javascript Algorithms And Data Structures Certification/Functional Programming/Implement the filter Method on a Prototype.js
+++ Javascript Algorithms And Data Structures Certification/Functional Programming/Implement the filter Method on a Prototype.js
@@ -1,18 +1,19 @@
 // the global Array
 var s = [23, 65, 98, 5];
 
-Array.prototype.myFilter = function(callback){
+Array.prototype.myFilter = function (callback) {
   var newArray = [];
   // Add your code below this line
-  for(let i=0; i<s.length; i++) {
-    if(callback(s[i]))
+  for (let i = 0; i < s.length; i++) {
+    if (callback(s[i])) {
       newArray.push(s[i]);
+    }
   }
   // Add your code above this line
   return newArray;
 
 };
 
-var new_s = s.myFilter(function(item){
+var new_s = s.myFilter(function (item) {
   return item % 2 === 1;
 });
