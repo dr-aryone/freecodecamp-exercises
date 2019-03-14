7e1dcb8a20d831533bdc5109a13eeda7a771d9f2
Phillip Shim <shimphillip@gmail.com>
2019-03-14 10:30:18 -0500

    Update Combine an Array into a String Using the join Method.js

--- Javascript Algorithms And Data Structures Certification/Functional Programming/Combine an Array into a String Using the join Method.js
+++ Javascript Algorithms And Data Structures Certification/Functional Programming/Combine an Array into a String Using the join Method.js
@@ -1,6 +1,6 @@
 function sentensify(str) {
   // Add your code below this line
-  return str.split(/[-.,]/).join(" ");
+  return str.split(/\W/).join(" ");
   
   // Add your code above this line
 }
