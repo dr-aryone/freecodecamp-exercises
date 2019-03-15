32d4b1dcaed2ef14a699e15d8c9ae6e5b19a5518
Phillip Shim <shimphillip@gmail.com>
2019-03-15 11:12:41 -0500

    Update Seek and Destroy.js

--- Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/Seek and Destroy.js
+++ Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/Seek and Destroy.js
@@ -1,18 +1,20 @@
-function destroyer() {
-  // Remove all the values
-  const args = Array.from(arguments);
-  const arr = args[0];
+function destroyer(arr, ...rest) {
+  const newArr = [];
+  let flag = true;
 
-  for(let i=0; i<arr.length; i++){
-    for(let j=1; j<args.length; j++){
-      if(arr[i] === args[j]){
-        arr.splice(i, 1);
-        i--;
+  arr.forEach(number1 => {
+    rest.forEach(number2 => {
+      if(number1 === number2) {
+        flag = false;
       }
+    })
+    if(flag) {
+      newArr.push(number1)
     }
-  }
+    flag = true;
+  })
 
-  return arr;
+  return newArr;
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
