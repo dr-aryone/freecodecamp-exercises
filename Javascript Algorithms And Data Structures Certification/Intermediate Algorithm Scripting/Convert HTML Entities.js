167a0b062198677e0a76bfeb5ea22ed211558e75
Phillip Shim <shimphillip@gmail.com>
2019-03-18 11:06:40 -0500

    Update Convert HTML Entities.js

--- Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/Convert HTML Entities.js
+++ Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/Convert HTML Entities.js
@@ -1,29 +1,22 @@
 function convertHTML(str) {
-  // &colon;&rpar;
+  const map = {
+    "&": "amp",
+    "<": "lt",
+    ">": "gt",
+    '"': "quot",
+    "'": "apos"
+  }
   let newStr = "";
 
-  for(let i=0; i<str.length; i++) {
-    newStr += getEntity(str[i]);
+  for (let char of str) {
+    if (map[char]) {
+      newStr += "&" + map[char] + ";";
+    } else {
+      newStr += char;
+    }
   }
 
   return newStr;
-}
-
-function getEntity(char) {
-    let obj = {
-    "&" : "&amp;",
-    "<" : "&lt;",
-    ">" : "&gt;",
-    '"' : "&quot;",
-    "'" : "&apos;"
-  }
-
-  for(let prop in obj) {
-    if(char === prop)
-      return obj[prop];
-  }
-
-  return char;
 }
 
 convertHTML("Dolce & Gabbana");
