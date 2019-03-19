9eff14b8f4b889cf96535aeb37264c8aaa9902f0
Phillip Shim <shimphillip@gmail.com>
2019-03-19 15:44:04 -0500

    Update Binary Agents.js

--- Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/Binary Agents.js
+++ Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/Binary Agents.js
@@ -1,12 +1,10 @@
 function binaryAgent(str) {
+  let finalStr = "";
   let arr = str.split(" ");
 
-  for(let i=0; i<arr.length; i++) {
-    arr[i] = parseInt(arr[i], 2);
-    arr[i] = String.fromCharCode(arr[i]);
-  }
-
-  return arr.join("");
+  return arr.reduce((accumulator, current) =>
+    accumulator + String.fromCharCode(parseInt(current, 2))
+    , "")
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
