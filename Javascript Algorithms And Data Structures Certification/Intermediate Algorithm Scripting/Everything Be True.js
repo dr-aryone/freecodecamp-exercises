7dccd69202acdf414276d03d17ce2ba01606e7ee
Phillip Shim <shimphillip@gmail.com>
2019-03-19 15:57:05 -0500

    Update Everything Be True.js

--- Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/Everything Be True.js
+++ Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/Everything Be True.js
@@ -1,13 +1,6 @@
 function truthCheck(collection, pre) {
-  // Is everyone being true?
-  let count = 0;
 
-  for(let i=0; i<collection.length; i++) {
-    if(collection[i].hasOwnProperty(pre) && collection[i][pre])
-      count++;
-  }
-
-  return count === collection.length;
+  return collection.every(object => object[pre])
 }
 
-truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+truthCheck([{ "user": "Tinky-Winky", "sex": "male" }, { "user": "Dipsy", "sex": "male" }, { "user": "Laa-Laa", "sex": "female" }, { "user": "Po", "sex": "female" }], "sex");
