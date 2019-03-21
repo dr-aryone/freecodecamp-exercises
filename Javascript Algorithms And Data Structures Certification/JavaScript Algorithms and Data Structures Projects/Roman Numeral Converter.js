6d4be3e7440f0e21b4dc348fe8ea8bdce2cf015f
Phillip Shim <shimphillip@gmail.com>
2019-03-21 10:31:24 -0500

    Update Roman Numeral Converter.js

--- Javascript Algorithms And Data Structures Certification/JavaScript Algorithms and Data Structures Projects/Roman Numeral Converter.js
+++ Javascript Algorithms And Data Structures Certification/JavaScript Algorithms and Data Structures Projects/Roman Numeral Converter.js
@@ -1,32 +1,30 @@
 function convertToRoman(num) {
-  let romans1 = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-  let romans10 = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-  let romans100 = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-  let romans1000 = ["", "M", "MM", "MMM"];
-  let str = num.toString();
-  let romanNumber = "";
-  let n = 0;
-
-  for(let i=str.length-1; i >= 0; i--) {
-      if(str.length-i === 1) {
-        n = Number(str[i]);
-        romanNumber = romans1[n] + romanNumber;
-      }
-      else if(str.length-i === 2) {
-        n = Number(str[i]);
-        romanNumber = romans10[n] + romanNumber;
-      }
-      else if(str.length-i === 3) {
-        n = Number(str[i]);
-        romanNumber = romans100[n] + romanNumber;
-      }
-      else {
-        n = Number(str[i]);
-        romanNumber = romans1000[n] + romanNumber;        
-      }
+  const numerals = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
   }
-  
- return romanNumber;
+
+  let roman = "";
+
+  for(let key in numerals) {
+    while(num >= numerals[key]) {
+      roman += key;
+      num -= numerals[key];
+    }
+  }
+
+ return roman;
 }
 
-convertToRoman(68);
+convertToRoman(36);
